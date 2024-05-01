20690d8a781842b2462896b264602fecc2984535
byte-0x74 <wexberkyhd@gmail.com>
2024-05-01 11:32:24 +0300

    [WIP] Spammer module and changes to min template

--- templates/minimal-template.js
+++ templates/minimal-template.js
@@ -1,5 +1,6 @@
 // Template version 0.0.4-dev
 // This is a minimal template for creating a module. It has a basic structure and a render function for the gui.
+// Modules have save system to save configured options. Each bot has its own save file for each module.
 // https://docs.sensum.cc
 
 /*Module Config
@@ -15,8 +16,29 @@ Module Config*/
 const gui = getGui();
 const bot = getBot();
 
+// This gets called when the module gets enabled
+// ConfigSave is the json data saved in onSave function you can load the config settings from it.
+function onLoad(configSave)
+{
+    
+}
+
+// This gets called when the module gets disabled
+function onUnload()
+{
+
+}
+
+// This gets called when module gets saved by user
+function onSave()
+{
+    // return json object with data to save
+}
+
 // This gets called in the modal render loop! only put gui related code here. dont put thread blocking code or operations here as it will pause the whole gui.
 // Use newThread for operations that take time.
-function render() {
-    
+// This function is optional and can be removed if not needed.
+function render()
+{
+
 }
